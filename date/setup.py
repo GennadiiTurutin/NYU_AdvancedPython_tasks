01597953292ae5fbca912b2df107d3c8a03fd73e
Gennadii Turutin <gennadii.turutin@gmail.com>
2018-05-19 16:52:40 -0700

    Update setup.py

--- date/setup.py
+++ date/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup( name='datetime_func',
        version='0.1',
        description='This module works with date formats',
-       url='',                # usually a github URL
+       url='https://github.com/GennadiiTurutin/NYU_AdvancedPython_tasks/blob/master/date/setup.py',      
        author='Gennadii Turutin',
        author_email='gennadii.turutin@gmail.com',
        license='MIT',
