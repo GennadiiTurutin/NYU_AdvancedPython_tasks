93672ffdd0682257a76a69acc29baba920d58af0
Gennadii Turutin <gennadii.turutin@gmail.com>
2018-05-20 14:46:33 -0700

    Update test_date_simple.py

--- date/test_date_simple.py
+++ date/test_date_simple.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
 
-# In[70]:
-
 import date_simple as ds
 import pytest
 import datetime as dt
@@ -49,62 +47,43 @@ def test_get_date_string_format_value(x):
         ds.get_date_string(_format = x)
         
 
-
-# In[2]:
-
 #1. assert that if date_simple.get_date_object() is called with no arguments, it will return a date object for today.
 test_get_date_object_args()
 
-
-# In[3]:
 
 #2. assert that if date_simple.get_date_object() is called with a properly formatted date string, it will return a date object for that date.
 test_get_date_object_string('2012-12-05')
 
 
-# In[17]:
-
 #3. assert that if date_simple.get_date_string() is called with no arguments, it will return a date string for today.
 test_get_date_string_args()
 
-
-# In[37]:
 
 #4. assert that if date_simple.get_date_string() is called with a date object, it will return the date string for that date.
 test_get_date_string(dt.datetime.strptime('02/13/2017', '%m/%d/%Y').date(), '%m/%d/%Y')
 
 
-# In[48]:
-
 #5.assert that if date_simple.get_date_string() is called with format='MM/DD/YYYY', it will return a date string in that format.
 test_get_date_string_extra_1(dt.datetime.strptime('02/13/2017', '%m/%d/%Y').date(), 'MM/DD/YYYY')
 
 
-# In[53]:
 
 #6. assert that if date_simple.get_date_string() is called with format='DD-Mon-YY', it will return a date string in that format.
 test_get_date_string_extra_2(dt.datetime.strptime('02-10-17', '%d-%m-%y').date(), 'DD-Mon-YY')
 
 
-# In[68]:
-
 #7. assert that if date_simple.get_date_object() is called with an improperly formatted string date, it will raise a ValueError exception.
 test_get_date_object_value('2012-12-05eee')
 
 
-# In[65]:
 
 #8. assert that if date_simple.get_date_object() is called with an object that is not type str, it will raise a TypeError exception.
 test_get_date_object_type(2342)
 
 
-# In[73]:
-
 #9. assert that if date_simple.get_date_string() is called with an object that is not type datetime.date, it will raise a TypeError exception.
 test_get_date_string_type(353453)
 
-
-# In[71]:
 
 test_get_date_string_format('sdfsgsg')
 
